8ab7eaa970ca3fed1d673ce21ff5b38bd8cccb54
Ichoku Chinonso <greatchinonso7@gmail.com>
2019-11-10 07:54:53 -0500

    Single room page completed

--- src/pages/SingleRoom.Component.jsx
+++ src/pages/SingleRoom.Component.jsx
@@ -51,8 +51,23 @@ export default class SingleRoom extends Component {
                             <h3>info</h3>
                             <h6>price: ${price}</h6>
                             <h6>size: {size} SQFT</h6>
+                            <h6>
+                                max capacity: {
+                                    capacity > 1 ? `${capacity} people` : `${capacity} person`
+                                }
+                            </h6>
+                            <h6>{pets ? 'Pets are allowed' : 'No pets are allowed'}</h6>             
+                            <h6>{breakfast && 'Free breakfast included'}</h6>             
                         </article>
                     </div>
+                </section>
+                <section className="room-extras">
+                    <h6>extras</h6>
+                    <ul className="extras">
+                        {extras.map((item, index) => {
+                            return <li key={index}>- {item}</li>
+                        })}
+                    </ul>
                 </section>
             </>
         )
