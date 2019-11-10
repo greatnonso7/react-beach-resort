c17ae70b4de7c3503cf11cc3e07af6352446903a
Ichoku Chinonso <greatchinonso7@gmail.com>
2019-11-10 18:50:34 -0500

    filter rooms using breakfast or pets or size

--- src/Context.js
+++ src/Context.js
@@ -57,7 +57,7 @@ class RoomProvider extends Component {
 
     handleChange = event => {
         const target = event.target
-        const value = event.target === 'checkbox' ? target.checked : target.value
+        const value = target.type === 'checkbox' ? target.checked : target.value
         const name = event.target.name
 
         this.setState({
@@ -76,6 +76,9 @@ class RoomProvider extends Component {
         /** Transform the value */
         capacity = parseInt(capacity)
 
+        /** Transform the price */
+        price = parseInt(price)
+
         /** filter by type */
         if(type !== 'all') {
             tempRooms = tempRooms.filter(room => room.type === type)
@@ -86,6 +89,23 @@ class RoomProvider extends Component {
             tempRooms = tempRooms.filter(room => room.capacity >= capacity)
         }
 
+        /** filter by price */
+        tempRooms = tempRooms.filter(room => room.price <= price)
+
+        /** filter by size */
+        tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+
+        /** filter by breakfast */
+        if(breakfast === true) {
+            tempRooms = tempRooms.filter(room => room.breakfast === true)
+        }
+
+        /** filter by pets */
+        if(pets === true) {
+            tempRooms = tempRooms.filter(room => room.pets === true)
+        }
+        
+        /** change state */
         this.setState({
             sortedRooms: tempRooms
         })
