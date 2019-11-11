6433e55d26ed930334c1c69df36acaa58bba3bb3
Ichoku Chinonso <greatchinonso7@gmail.com>
2019-11-10 22:34:23 -0500

    created an env file for my access token

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful'
 
+
 export default createClient({
-    space: "sl7qrudwgecj",
-    accessToken: "IooQ6v_-_TAtYPOHdDvkhMqfPJ-Av_hMclU_MQR985o" 
-})
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN 
+});
